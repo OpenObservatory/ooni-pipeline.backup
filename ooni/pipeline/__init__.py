e58de458ae887568e42833456c781cb07507c585
otr <otr@bockcay.de>
2014-10-26 13:58:36 +0100

    Avoid reading in env variables again and just rely upon settings module

--- ooni/pipeline/__init__.py
+++ ooni/pipeline/__init__.py
@@ -2,15 +2,7 @@ import sys
 import os
 
 from ooni.pipeline import task
-
-db_ip, db_port = os.environ['OONI_DB_IP'], int(os.environ['OONI_DB_PORT'])
-bridge_db_filename = os.environ['OONI_BRIDGE_DB_FILE']
-bridge_by_country_output = os.path.join(os.environ['OONI_PUBLIC_DIR'],
-                                        'bridges-by-country-code.json')
-sanitised_directory = os.environ['OONI_SANITISED_DIR']
-remote_servers_file = os.environ['OONI_REMOTE_SERVERS_FILE']
-raw_directory = os.environ['OONI_RAW_DIR']
-
+from ooni.pipeline import settings
 
 def usage():
     print("""
@@ -23,17 +15,15 @@ Task names:
 """ % sys.argv[0])
     sys.exit(1)
 
-
 def run(task_name):
     if task_name == "export":
-        task.export.main(bridge_db_filename, bridge_by_country_output)
+        task.export.main(settings.bridge_db_filename, settings.bridge_by_country_code_output)
     elif task_name == "sanitise":
         task.sanitise.main()
     elif task_name == "import":
         task.publish.main()
     elif task_name == "sync":
-        task.sync.main(raw_directory, sanitised_directory, remote_servers_file,
-                       db_ip, db_port)
+        task.sync.main()
     else:
         print("Invalid command!")
 
