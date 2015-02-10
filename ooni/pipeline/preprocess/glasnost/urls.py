93b3f8297ef73dca99ae9567e1ffdb41db196822
Simone Basso <bassosimone@gmail.com>
2015-02-10 13:45:56 +0100

    Remove unneeded glasnost/urls.py file

--- ooni/pipeline/preprocess/glasnost/urls.py
+++
@@ -1,9 +0,0 @@
-# Glasnost Parser v2.
-# Developed 2011/2012 by Hadi Asghari (http://deeppacket.info)
-
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('parser.views',
-    url(r'^/do$','ProcessLogs', name='main_process_logs'),
-)
-
